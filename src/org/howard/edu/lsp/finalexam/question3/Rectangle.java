bf4c38cc05bcd05e27f6e742abfbea4fa02c1c41
n1ckc0de <118502739+n1ckc0de@users.noreply.github.com>
2024-12-05 20:36:33 -0500

    Create Rectangle.java

---
+++ src/org/howard/edu/lsp/finalexam/question3/Rectangle.java
@@ -0,0 +1,8 @@
+package org.howard.edu.lsp.finalexam.question3;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
