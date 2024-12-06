178de189ff14d1031823ab76c7c90392c983b69e
n1ckc0de <118502739+n1ckc0de@users.noreply.github.com>
2024-12-05 20:30:02 -0500

    Create RandomNumberStrategy.java

---
+++ src/org/howard/edu/lsp/finalexam/question2/RandomNumberStrategy.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.finalexam.question2;
+
+/**
+ * Interface for random number generation strategies.
+ */
+public interface RandomNumberStrategy {
+    /**
+     * Generates a random number.
+     * @return a random positive integer
+     */
+    int generateRandomNumber();
+}
